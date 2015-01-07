a066b4cffc68cb67d0d7922dc025dede3b9a8807
Hayden Guthrie <seehaydeng@gmail.com>
2015-01-06 19:37:55 -0800

    First lecture

---
+++ index.php
@@ -0,0 +1,62 @@
+class Team {
+
+//class body
+//class body
+//class body
+
+}
+
+$team1 = new Team();
+$team2 = new Team();
+
+class Team {
+public $name    = "Ducks";
+public $rank    = "1st";
+public $area    = "Anaheim";
+public $colors  = "Gold,black,Orange";
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Team {
+
+//class body
+//class body
+//class body
+
+}
+
+$team1 = new Team();
+$team2 = new Team();
+
+class Team {
+protected $name    = "Kings";
+protected $rank    = "4th";
+protected $area    = "LA";
+protected $colors  = "Black,White";
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Team {
+
+//class body
+//class body
+//class body
+
+}
+
+$team1 = new Team();
+$team2 = new Team();
+
+class Team {
+private $name    = "Sharks";
+private $rank    = "3d";
+private $area    = "San Jose";
+private $colors  = "Blue";
+}
+
+
+
+
+
