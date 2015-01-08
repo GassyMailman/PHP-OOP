352f1aea8b3cd2422c97f31ffd45765e06da8b5a
Hayden Guthrie <seehaydeng@gmail.com>
2015-01-07 20:12:41 -0800

    Lecture 2

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ public $area    = "Anaheim";
 public $colors  = "Gold,black,Orange";
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////
 
 class Team {
 
@@ -36,7 +36,7 @@ protected $area    = "LA";
 protected $colors  = "Black,White";
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////
 
 class Team {
 
@@ -56,7 +56,59 @@ private $area    = "San Jose";
 private $colors  = "Blue";
 }
 
+///////////////////////////////////////////////////////////////Lecture__2////////////////////////////////////////////////////////////////////////////////////////
 
 
+class Team {
+public $teamname;
+public $area;
+public $colors;
 
+		function __construct($title, $teamname, $area, $colors) {
+		$this­>teamname = $teamname;
+		$this­>area = $area;
+		$this­>colors = $colors;
+		}
 
+		function getInfo() {
+		return “{$this­>teamname}” .
+		“{$this­>area}”;
+		}
+
+}
+
+class Car {
+public $name;
+public $manufactur;
+public $year;
+
+		function __construct($title, $name, $lastName, $year) {
+		$this­>name = $name;
+		$this­>manufactur = $manufactur;
+		$this­>year = $year;
+		}
+
+		function getCarinfo() {
+		return “{$this­>name}” .
+		“{$this­>manufactur}”;
+		}
+
+}
+
+class Book {
+public $title;
+public $author;
+public $publisher;
+
+		function __construct($title, $title, $author, $publisher) {
+		$this­>title = $title;
+		$this­>author = $author;
+		$this­>publisher = $publisher;
+		}
+
+		function getBook() {
+		return “{$this­>title}” .
+		“{$this­>author}”;
+		}
+
+}
